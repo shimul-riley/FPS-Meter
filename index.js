8e8b7f89053ce1b2f33869a9b7ba0a47d4a0983f
Shimul Ghost Riley <108178505+shimul-riley@users.noreply.github.com>
2023-10-01 22:51:03 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript for responsive hamburger menu
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
